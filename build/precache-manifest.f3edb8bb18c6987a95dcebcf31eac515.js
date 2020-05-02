4704f45b6df422bc30e95ba974c26397cb867f12
gabrieltsouza <gtsouza@mandic.com.br>
2020-05-02 02:15:50 -0300

    Atualizando o projeto

---
+++ build/precache-manifest.f3edb8bb18c6987a95dcebcf31eac515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "759412629bca1552edd3",
+    "url": "/static/js/main.494eb743.chunk.js"
+  },
+  {
+    "revision": "ea7c166984c95365a53f",
+    "url": "/static/js/2.fdb27254.chunk.js"
+  },
+  {
+    "revision": "759412629bca1552edd3",
+    "url": "/static/css/main.0ba467df.chunk.css"
+  },
+  {
+    "revision": "5b2e49501cd6507a455b3f14c538be41",
+    "url": "/index.html"
+  }
+];
